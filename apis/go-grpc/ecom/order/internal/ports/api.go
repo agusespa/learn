84fbeb5740fc4873c1dd01378a8607a272c58476
Agustin Espana <agustinespania@gmail.com>
2023-10-03 14:21:06 +0200

    Clean up

--- apis/go-grpc/ecom/order/internal/ports/api.go
+++
@@ -1,8 +0,0 @@
-package ports
-
-import "order/internal/application/domain"
-
-type APIPort interface {
-	PostOrder(domain.Order) (domain.Order, error)
-	GetOrder(int64) (domain.Order, error)
-}
--- apis/go-grpc/ecom/order/internal/ports/db.go
+++
@@ -1,8 +0,0 @@
-package ports
-
-import "order/internal/application/domain"
-
-type DBPort interface {
-	ReadOrder(int64) (domain.Order, error)
-	CreateOrder(domain.Order) (domain.Order, error)
-}
