1d9af478bc90fb67912decf9ae65a75fb2577424
Agustin Espana <agustinespania@gmail.com>
2024-02-21 15:55:15 +0100

    feat: added vr capabilities

--- web-fe/threejs/vr/main.js
+++ web-fe/threejs/vr/main.js
@@ -1,2 +1,38 @@
 import * as THREE from "three";
-import { XRButton } from "three/addons/webxr/XRButton.js";
+import { VRButton } from "three/addons/webxr/VRButton.js";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.xr.enabled = true;
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+document.body.appendChild(VRButton.createButton(renderer));
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// 1. Ambient Light (Soft overall lighting)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+// 2. Directional Light (Simulates sunlight)
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+directionalLight.position.set(1, 2, 3);
+scene.add(directionalLight);
+
+camera.position.z = 5;
+
+renderer.setAnimationLoop(function () {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+});
